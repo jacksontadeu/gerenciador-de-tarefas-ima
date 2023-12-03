09a8924d591a8427b47125f48ec99a794a80b111
Jackson Moraes <jacksontadeu@hotmail.com>
2023-12-03 15:10:58 -0300

    :lipstick: feat: Implementado o cadastro de tarefas salvando em localstorage

--- scripts.js
+++ scripts.js
@@ -43,5 +43,28 @@ function logout(){
     localStorage.removeItem('contaLogada')
     location.href='index.html'
 }
+function cadastrarTarefas(){
+    let tarefa = document.getElementById('tarefa').value;
+    let dataInicio = document.getElementById('data-inicio').value;
+    let horaInicio = document.getElementById('hora-inicio').value;
+    let dataTermino = document.getElementById('data-termino').value;
+    let horaTermino = document.getElementById('hora-termino').value;
+    let descricao = document.getElementById('descricao').value;
+    
+    let compromisso = {
+        "conta": localStorage.getItem("contaLogada"),
+        "tarefa": tarefa,
+        "dataInicio": dataInicio,
+        "horainicio": horaInicio,
+        "datatermino": dataTermino,
+        "horatermino": horaTermino,
+        "descricao": descricao
+    }
+    var compromissos = JSON.parse(localStorage.getItem('tarefas')|| '[]');
+    compromissos.push(compromisso)
+    localStorage.setItem('tarefas', JSON.stringify(compromissos))
+    location.href='adicionar_tarefas.html'
+
+}
 var contaLogin = document.getElementById('conta-logada')
 contaLogin.innerHTML = `Olá ${localStorage.getItem('contaLogada')} seja bem vindo!!!`
